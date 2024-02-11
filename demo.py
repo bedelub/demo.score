efb8a33f5807d087e2b65c7adb2fd4eb2d695dde
bedelu <bedelu2007@gmail.com>
2024-02-11 09:23:52 -0600

    first comnit

---
+++ demo.py
@@ -0,0 +1,24 @@
+def largestnumber(numbers) :
+ 
+    biggest_num = numbers [0]
+
+    for num in numbers:
+       if num > biggest_num:
+          biggest_num = num
+
+    return biggest_num
+
+
+print{largestnumber['name': 'Alemayehu', 'score': 85},
+ {'name': 'Nathan', 'score': 92},
+ {'name': 'Azeb', 'score': 78},
+ {'name': 'Hailu', 'score': 95},
+ {'name': 'Gete', 'score': 88},
+ {'name': 'Abigiya', 'score': 75},
+ {'name': 'Tesfa', 'score': 90},
+ {'name': 'Desta', 'score': 82},
+ {'name': 'Tsinat’, 'score': 89},
+ {'name':  'score': 93]}
+
+
+
